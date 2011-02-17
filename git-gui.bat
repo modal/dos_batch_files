f5d50b328f479214e1b393df6c5b5c41e55427b7
Tim Dahlin <duhlynn@gmail.com>
2011-02-17 16:31:48 -0600

    initial commit

---
+++ git-gui.bat
@@ -0,0 +1,8 @@
+@echo off
+rem This script allows one to pull up a git gui for the current directory
+rem REQUIRES
+rem msysgit installation
+rem activestate tcl for wish.exe 
+
+start c:\tcl\bin\wish.exe %gitdir%\libexec\git-core\git-gui %*
+rem start c:\tcl\bin\wish.exe c:\bin\git\msysgit\libexec\git-core\git-gui 
