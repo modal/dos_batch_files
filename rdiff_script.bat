c8569b8e903f1fe0275cd835f955c772fb1efa16
Tim Dahlin <duhlynn@gmail.com>
2011-04-08 15:57:18 -0500

    added notes and error message if rdiff drive not connected

--- rdiff_script.bat
+++ rdiff_script.bat
@@ -22,14 +22,31 @@ rem
 rem Notes:
 rem
 rem Nest folders up to Base folder must be created for rdiff-backup to work.
+rem schtask command can be used for scheduling tasks.
+rem http://ss64.com/nt/schtasks.html
+rem http://support.microsoft.com/kb/308569
+rem http://drupal.org/node/31506
+rem
 rem
 rem Maintenance:
 rem
-rem Periodically removed old incremental saves and verify the
-rem backup.
+rem Periodically remove old incremental saves and verify the backup.
 rem
 rem **************************************************************************
 
+IF EXIST G: GOTO :GEXISTS
+echo ############################
+echo  eee  RR    RR    OO    RR
+echo E    R  R  R  R  O  O  R  R
+echo Eee  RRR   RRR   O  O  RRR
+echo E    R  R  R  R  O  O  R  R
+echo Eeee R   R R   R  OO   R   R
+echo ############################
+echo ERROR: G DRIVE NOT CONNECTED, RDIFF-BACKUP SCRIPT NOT RUN
+pause
+exit
+
+:GEXISTS
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\Keil\C51\projects g:\rd_backup\Keil\C51\projects
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\_bkspc g:\rd_backup\_bkspc
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\adsp g:\rd_backup\adsp
@@ -37,6 +54,10 @@ rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\bin g:\r
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\home g:\rd_backup\home
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\projects g:\rd_backup\projects
 rdiff-backup --print-statistics --verbosity 5 --terminal-verbosity 5 c:\repo g:\rd_backup\repo
+
+
+
+pause
 
 rem **************************************************************************
 rem Useful rdiff-backup commands
